bcc90d586f01e45a5f902d43e8b4170ce1bbea42
Johann <johannholzmann@hotmail.com>
2022-05-26 13:21:51 -0300

    Add ThemeProvider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
+import {ThemeProvider} from 'next-themes';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
