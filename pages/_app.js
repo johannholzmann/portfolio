040fc067354a4138c57eb52a4bc84d3091855481
Johann <johannholzmann@hotmail.com>
2022-05-27 09:19:53 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
 import { ThemeProvider } from 'next-themes';
-import Layout from '../components/layout';
+import Layout from '../components/layout/layout.js';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
