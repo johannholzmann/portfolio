349577b28d650f9e52590cc0f5d54772022b6e21
Johann <johannholzmann@hotmail.com>
2022-05-28 03:37:53 -0300

    Fix size themetoggle

--- components/themetoggle/ThemeToggle.js
+++ components/themetoggle/ThemeToggle.js
@@ -1,23 +1,27 @@
 import { useTheme } from 'next-themes'
 import { Button } from 'react-bootstrap';
 
+
 import styles from './themetoggle.module.css'
+
 
 const ThemeChanger = () => {
   const { theme, setTheme } = useTheme()
 
   return (
-    <button className={styles.toggle_button} type="buton" onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}>
-      {theme === 'dark' ?
 
-        (
-          <span>☀️</span>
-        ) :
-        (
-          <span>🌙</span>
-        )
-      }
-    </button>
+      <button className={styles.toggle_button} type="buton" onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}>
+        {theme === 'dark' ?
+
+          (
+            <span>☀️</span>
+          ) :
+          (
+            <span>🌙</span>
+          )
+        }
+
+      </button>
   )
 }
 
