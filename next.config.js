4144b053480259f37fa40199e6970eb9378a1280
Johann <johannholzmann@hotmail.com>
2022-06-01 20:35:51 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    URL: "https://portfolio-johann-express.herokuapp.com/",
-  }
 }
 
 module.exports = nextConfig
