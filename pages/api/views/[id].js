609063a55e718008b87f3ee3096d11d7a1d40263
Johann <johannholzmann@hotmail.com>
2022-06-02 00:34:42 -0300

    Add try catch on get data from database

--- pages/api/views/[id].js
+++ pages/api/views/[id].js
@@ -2,22 +2,36 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-    const id = req.query.id;
+    let output = null;
+    try {
+        const id = req?.query?.id;
+        if (!id)
+            throw new Error("ID null");
 
-    //upsert update views with a specific id, or create that view record if it does noe exists
-    const views = await prisma.views.upsert({
-        where: { 
-            id: id 
-        },
-        update:{
-            count:{
-                increment: 1,
+        //upsert update views with a specific id, or create that view record if it does noe exists
+        const views = await prisma.views.upsert({
+            where: {
+                id: id
             },
-        },
-        create:{
-            id: id,
-            count: 1,
-        },
-    })
-    res.json(views.count);
+            update: {
+                count: {
+                    increment: 1,
+                },
+            },
+            create: {
+                id: id,
+                count: 1,
+            },
+        })
+        const ret = views?.count;
+        if (!ret)
+            throw new Error("Not exists record");
+
+        output = ret;
+
+    }
+    catch (error) {
+        //console.log("Error: ", error);
+    }
+    return res.json(output);
 }
