f54e260d5fe74bee8030d17e7447b6ad764bfeda
Johann <johannholzmann@hotmail.com>
2022-06-02 20:16:02 -0300

    Add pepas page

---
+++ pages/pepas/index.js
@@ -0,0 +1,11 @@
+import Pepas from "../../components/pepas/index.js";
+
+export default function Index() {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1> Pepas</h1>
+            <br />
+            <Pepas />
+        </div>
+    )
+}
