c49c25fb735bb5c10579d6829a2abc5abd1edbe2
Johann <johannholzmann@hotmail.com>
2022-06-05 02:56:57 -0300

    Add icons to footer and remove logo

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,38 +1,44 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import Link from 'next/link';
 import styles from "./footer.module.css";
 
 import Logo from "../logo/logo";
 import View from "../views/views";
+import React from "react";
 
+const width_icons = 50;
 function Footer() {
     return (
         <div className={styles.footer_container}>
             <footer className={styles.footer}>
-                <Col>
-                    <a href="https://www.linkedin.com/in/johann-holzmann/" target="_blank" rel="noopener noreferrer">
-                        Linkedin
-                    </a>
-                    <a href="https://github.com/johannholzmann" target="_blank" rel="noopener noreferrer">
-                        Github
-                    </a>
-                </Col>
-                <Col sm={12} className={styles.col_views}>
-                    <View />
-                </Col>
-                <Col sm={12} className={styles.col_data_footer}>
-                    <Link href="/">
-                        <a>
-                            {'Copyright © Portfolio '}
-                            {new Date().getFullYear()}
-                            {' '}
-                            <div className={styles.logo_container}>
-                                <Logo />
-                            </div>
-                            {' '}
+                <Row style={{ backgroundColor: 'sa', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Col xs={2} sm={1}>
+                        <a href="https://www.linkedin.com/in/johann-holzmann/" target="_blank" rel="noopener noreferrer">
+                            <Image src="icons8-linkedin-circled-240.png" width={width_icons} />
                         </a>
-                    </Link>
-                </Col>
+                    </Col>
+                    <Col xs={2} sm={1}>
+                        <a href="https://github.com/johannholzmann" target="_blank" rel="noopener noreferrer">
+                            <Image src="icons8-github-240.png" width={width_icons} />
+                        </a>
+                    </Col>
+                    <Col xs={2} sm={1}>
+                        <a href="mailto:johannholzmann@hotmail.com" target="_blank" rel="noopener noreferrer">
+                            <Image src="icons8-envelope-96.png" width={width_icons} />
+                        </a>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={12} className={styles.col_views}>
+                        <View />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={12} className={styles.col_copyright}>
+                        {'Copyright © Portfolio '}
+                        {new Date().getFullYear()}
+                    </Col>
+                </Row>
             </footer>
         </div>
     );
