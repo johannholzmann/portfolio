fc750aec52246f2696c9766111ed2e76700f56cf
Johann <johannholzmann@hotmail.com>
2022-12-21 20:27:28 -0300

    Update userinformation.js

--- components/userinformation/userinformation.js
+++ components/userinformation/userinformation.js
@@ -5,13 +5,12 @@ import Avatar from '../avatar/avatar';
 const InformationUser = ({ }) => {
   return (
     <Container>
-
       <Row>
-        <Col sm={12} md={8}>
+        <Col sm={12} md={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: "center" }}>
           <h2 className={styles.h1_name}>Johann Holzmann</h2>
           <h4>Estudiante de Ingeniería en Computación en la Universidad Nacional del Sur</h4>
         </Col>
-        <Col sm={12} md={4} style={{'justify-content':'center', 'align-items':'center', display:'flex'}}>
+        <Col sm={12} md={4} style={{ 'justify-content': 'center', 'align-items': 'center', display: 'flex' }}>
           <Avatar />
         </Col>
       </Row>
