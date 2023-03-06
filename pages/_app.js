bee61ffd6ed3466c10f5a3ee2947f01d47162776
Johann <johannholzmann@hotmail.com>
2023-03-06 02:41:34 -0300

    Create styles/tailwind.css and include on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import "../styles/tailwind.css"
 
 import { ThemeProvider } from 'next-themes';
 import Layout from '../components/layout/layout.js';
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme='dark'>
+    <ThemeProvider defaultTheme='dark' attribute='class'>
       <Layout>
         <Component {...pageProps} />
       </Layout>
