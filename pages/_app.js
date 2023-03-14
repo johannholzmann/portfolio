144979fb550ca6c89fae69d7dbe34fbdfa97e486
Johann <johannholzmann@hotmail.com>
2023-03-14 10:34:21 -0300

    Add Analytics on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,14 @@ import "../styles/tailwind.css"
 
 import { ThemeProvider } from 'next-themes';
 import Layout from '../components/layout/layout.js';
+import { Analytics } from "@vercel/analytics/react"
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme='dark' attribute='class'>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </ThemeProvider>
   )
