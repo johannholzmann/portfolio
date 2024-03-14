32a4514696c0e4fbc446b15a4faebd502bf58e96
Johann <johannholzmann@hotmail.com>
2024-03-14 19:18:25 -0300

    Some changes

--- components/technology.tsx
+++ components/technology.tsx
@@ -1,4 +1,4 @@
-export default function Technology({ name }) {
+export default function Technology({ name }: { name: string }) {
     return (
         <div className="w-80 hover:scale-[105%] duration-200 bg-white hover:bg-slate-100 border border-gray-200 rounded-lg shadow dark:bg-gray-700/90 dark:hover:bg-gray-800/90 dark:border-gray-700 text-black">
             <div className="p-5">
