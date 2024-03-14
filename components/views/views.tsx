510d96605fb19722ab511748530883283cc61bdd
Johann <johannholzmann@hotmail.com>
2024-03-14 19:45:30 -0300

    Revert "Convert views client component to react server component"
    This reverts commit e43b84593af631ff1d726d107728e10716dbf270.

--- components/views/views.tsx
+++ components/views/views.tsx
@@ -1,23 +1,28 @@
+"use client";
+
+import useSWR from "swr";
 import Spinner from "../spinner";
 
-async function fetcher(): Promise<{ views: number } | null> {
-    const res = await fetch(`${process.env.VERCEL_URL}/api/views/`, {
+async function fetcher(): Promise<{ views: number }> {
+    const res = await fetch(`api/views/`, {
         method: 'GET',
     });
     if (!res.ok)
-        return null;
+        throw new Error("Error al obtener vistas")
     const views = await res.json();
     return views;
 }
 
-export default async function View() {
-    const data = await fetcher();
-    if (data) {
+export default function View() {
+    const { data: views, error } = useSWR([`api/views/`], fetcher, {
+        revalidateOnFocus: false,
+    });
+    if (!error) {
         return (
             <div className="flex justify-center">
                 {
-                    data ?
-                        <span>{data.views} {data.views == 1 ? "visita" : "visitas"} al sitio</span>
+                    views ?
+                        <span>{views.views} {views.views == 1 ? "visita" : "visitas"} al sitio</span>
                         :
                         <Spinner />
                 }
